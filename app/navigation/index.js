3a6d4d3a89940cecad1e52c9a99bfb91f9ca4eda
TJ Grist <tjgrst@gmail.com>
2017-09-25 16:30:43 -0700

    removed second tab

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -1,17 +1,9 @@
 import { TabNavigator, StackNavigator } from 'react-navigation'
 
-import WelcomeScreen from '../containers/welcome_screen'
-import CheckinScreen from '../containers/checkin_screen'
 import PortfoliosScreen from '../containers/portfolios-screen'
 import NewPortfolioScreen from '../containers/new-portfolio-screen'
 import PortfolioDetailScreen from '../containers/portfolio-detail-screen'
 import CoinDetailScreen from '../containers/coin-detail-screen'
-
-const WelcomeTab = StackNavigator({
-  WelcomeScreen: { screen: WelcomeScreen },
-  CheckinScreen: { screen: CheckinScreen },
-
-});
 
 const PortfoliosTab = StackNavigator({
   PortfoliosScreen: { screen: PortfoliosScreen, },
@@ -22,7 +14,6 @@ const PortfoliosTab = StackNavigator({
 
 const Navigation = TabNavigator({
   Portfolios: { screen: PortfoliosTab },
-  Home: { screen: WelcomeTab },
 });
 
 export default Navigation
